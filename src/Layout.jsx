8284dac3497ddbb656f7ff7c49ea9315db77fb9d
nikhil.hedukar <nikhil.hedukar@laminaar.in>
2024-01-31 23:43:09 +0530

    Header and Footer components importing from index

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import Footer from "./compnenents/Footer/Footer";
-import Header from "./compnenents/Header/Header";
+import { Header, Footer } from "./compnenents";
 
 function Layout() {
   return (
